8cf6a89d97d58e0fc073af20e0993020735cbe1c
Wen Yao Chang <cha16034@byui.edu>
2017-07-14 16:16:35 -0600

    f

--- final-project/final-project.js
+++ final-project/final-project.js
@@ -1,67 +1,8 @@
-/*
-function getItem(instructions, id) {
-    for (var i = 0; i < instructions.length; i++)
-        {
-          if ( instructions[i].id == id)
-              //The i here would be 0 when the id is one,
-              //    i here would be 1 when the id is two.
-              return instructions[i];
-        }
-     
-    
-}
-//getItem: gets an item from a list by id
-//requires the list and id of the desired element
 
-function updateElement(elementID,instruction){
-    document.getElementById(elementID).innerHTML = instruction;
-}
-//updateElement: updates the contents of an element on the screen
-//requires the id of the div to update, and the new contents.
-
-function updateButton(buttonId,choiceId){
-
-    var button = document.getElementById(buttonId);
-    button.setAttribute('onclick',"nextStep(" + choiceId + ")")
-}
-//updateButton: sets the onclick event for a button with the id of the item it chooses
-
-function reset(){
-    nextStep(1);
-    document.getElementById("buttonOne").disabled = false;
-    document.getElementById("buttonTwo").disabled = false;
-}
-// This reset function would reset the content after the story ends.
-
-function nextStep(id) {
-    
-    var instruction = getItem(instructions, id);
-    
-    updateElement('title', instruction.title);
-    updateElement('description', instruction.description);
-
-    var choice1 = getItem(instructions, instruction.choices.first);
-    var choice2 = getItem(instructions, instruction.choices.second);
-    // get the items for choice1 and 2 from the list
-    
-    if (instruction.end){
-        window.alert("This is the end of the game!");
-        document.getElementById("buttonOne").disabled = true;
-        document.getElementById("buttonTwo").disabled = true;
-    }
-    // The if statement would test if the game reach the end point.
-    
-    updateElement('choiceOne', choice1.choiceText);
-    updateElement('choiceTwo', choice2.choiceText);
-    updateButton('buttonOne', choice1.id);
-    updateButton('buttonTwo', choice2.id);   
-}
-*/
- 
 
 
 // Current Location Scripts
-//(function () {
+(function () {
     
     function getJSON(url) {
         return fetch(url)
@@ -76,16 +17,16 @@ function nextStep(id) {
     // Get the data from the wunderground API
     function getData() {
     
-        getJSON("schedule.json").then(function (data) {
+        getJSON("/final-project/schedule.json").then(function (data) {
             //add the code necessary here to update the page with all of the correct data points.
             var data = data;
             console.log(data);
-            
+            var game1 = data.game1;
             
             
           //  document.getElementById("round").innerHTML = curLocation.city;
             
-            document.getElementById("date").innerHTML = data.game1.time;
+            document.getElementById("date").innerHTML = game1.time;
        /*     
             document.getElementById("location").innerHTML = condition.weather;
             
@@ -103,6 +44,6 @@ function nextStep(id) {
             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
         });
     }*/
-//}());
+}());
 //still dont know why
 
